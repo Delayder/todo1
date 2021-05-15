54f5be9c09f8ffa7385597ad111a4d8302df1829
Leider <ljdnieto@gmail.com>
2021-05-15 14:38:10 -0500

    first commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,10 @@
+export class STORAGE_KEY {
+    public static readonly AUTH: string = "";
+}
+
+export class URL_KEY_ROUTES{
+    public static readonly LOCAL_ROOT: string = "";
+    public static readonly DEV_ROOT: string = "";
+
+    public static readonly AUTH: string = "";
+}
